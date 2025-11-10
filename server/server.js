a0787b05b148157e383f3cf6b67efa57df8c3e66
Dananjaya Thathsara <saddthathsara@gmail.com>
2025-11-10 20:22:56 +0530

    update server js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ import chatRouter from './routes/chatRoutes.js'
 import messageRouter from './routes/messageRoutes.js'
 import creditRouter from './routes/creditRoutes.js'
 import { stripeWebhooks } from './controllers/webhooks.js'
+import path from "path"
+import { fileURLToPath } from "url"
 
 const app = express()
 
@@ -27,6 +29,17 @@ app.use('/api/message', messageRouter)
 app.use('/api/credit', creditRouter)
 
 const PORT = process.env.PORT || 3000
+
+//production
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const clientBuildPath = path.join(__dirname, "../client/dist");
+app.use(express.static(clientBuildPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(clientBuildPath, "index.html"));
+});
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
