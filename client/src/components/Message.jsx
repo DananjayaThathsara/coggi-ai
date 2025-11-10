1964f57e693d67e9a3e66a6b4db81d1a2451f635
Dananjaya Thathsara <saddthathsara@gmail.com>
2025-11-10 14:04:50 +0530

    first commit

---
+++ client/src/components/Message.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react'
+import { assets } from '../assets/assets'
+import moment from 'moment'
+import Markdown from 'react-markdown'
+import Prism from 'prismjs'
+
+const Message = ({message}) => {
+
+  useEffect(()=>{
+    Prism.highlightAll()
+  },[message.content])
+
+  return (
+    <div>
+      {message.role === "user" ? (
+        <div className="flex items-start justify-end my-4 gap-2">
+          <div className="flex flex-col gap-2 p-2 px-4 bg-slate-50 dark:bg-[#007BFF]/30 border border-[#007BFF]/30 rounded-md max-w-2xl">
+            <p className="text-m dark:text-white">{message.content}</p>
+            <span className="text-xs text-gray-400 dark:text-[#B1A6C0]">
+              {moment(message.timestamp).fromNow()}
+            </span>
+          </div>
+          <img src={assets.user_icon} alt="" className="w-8 rounded-full" />
+        </div>
+      ) : (
+        <div className="inline-flex flex-col gap-2 p-2 px-4 max-w-2xl bg-[#007BFF]/20 dark:bg-[#007BFF]/30 border border-[#007BFF]/30 rounded-md my-4">
+          {message.isImage ? (
+            <img
+              src={message.content}
+              alt=""
+              className="w-full max-w-md mt-2 rounded-md"
+            />
+          ) : (
+            <div className="text-m dark:text-white reset-tw">
+              <Markdown>{message.content}</Markdown>
+            </div>
+          )}
+          <span className="text-xs text-gray-400 dark:text-[#B1A6C0]">
+            {moment(message.timestamp).fromNow()}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Message
---
+++ readme.md
@@ -0,0 +1,13 @@
+# 🧠 MovaAI
+
+MovaAI is an intelligent, AI-powered chat platform built with **React**, **Express.js**, **Node.js**, and **MongoDB**.  
+It provides real-time chat, user authentication, and smart assistant capabilities — designed for scalability, modularity, and modern development workflows.
+
+## 🚀 Features
+
+- 🔐 **User Authentication** — Secure login & registration using JWT tokens.
+- 🤖 **AI Assistant** — Smart responses via integrated OpenAI API.
+- 📡 **RESTful API** — Clean and scalable backend structure using Express.js.
+- 💾 **MongoDB Integration** — Robust data handling with Mongoose ODM.
+- ⚙️ **Environment Configurable** — `.env` support for environment-specific settings.
+- 🌐 **CORS Enabled** — Fully cross-origin ready for React frontend.
