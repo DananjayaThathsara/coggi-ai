849793bcb1c5aa2c800f5a6ddece01fa83e240c4
Dananjaya Thathsara <saddthathsara@gmail.com>
2025-11-11 00:08:24 +0530

    update server.js file`

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const clientBuildPath = path.join(__dirname, "../client/dist");
 app.use(express.static(clientBuildPath));
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(clientBuildPath, "index.html"));
 });
 
